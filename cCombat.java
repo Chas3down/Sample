31d1cf86c703bbd4695246be6670c3ab62032249
Chas3down <Chas3down@gmail.com>
2014-03-24 18:27:27 -0400

    Update cCombat.java

--- cCombat.java
+++ cCombat.java
@@ -1,3 +1,4 @@
+
 import org.parabot.environment.api.utils.Filter;
 import org.parabot.environment.scripts.Script;
 import org.parabot.environment.scripts.framework.Strategy;
@@ -7,12 +8,13 @@ import org.rev317.api.methods.*;
 import org.rev317.api.wrappers.hud.Item;
 import org.rev317.api.wrappers.hud.Tab;
 import org.rev317.api.wrappers.interactive.Npc;
+import org.rev317.api.wrappers.scene.SceneObject;
 
 
 import java.util.ArrayList;
 
 
-@ScriptManifest(author = "Chas3down", category = Category.COMBAT, description = "Kill Shit", name = "Kill Shit", servers = { "PkHonor" }, version = 1)
+@ScriptManifest(author = "Chas3down", category = Category.OTHER, description = "Kill Shit", name = "Kill Shit", servers = { "PkHonor" }, version = 1)
 public class cCombat extends Script{
 
     private final ArrayList<Strategy> strategies = new ArrayList<Strategy>();
@@ -32,6 +34,7 @@ public class cCombat extends Script{
         strategies.add(new Attack());
         strategies.add(new Eat());
         strategies.add(new Drink());
+        //strategies.add(new Banks());
 
         provide(strategies);
 
@@ -49,17 +52,18 @@ public class cCombat extends Script{
 
         @Override
         public boolean activate() {
+            if (Npcs.getNearest(MonsterID) != null){
+                for(Npc i : Npcs.getNearest(MonsterID)) {
+                  if(i != null && i.getDef().getId() != 0) {
+                     if(!i.isInCombat()){
+                         return Inventory.getCount(FoodID) > 1
+                                    && !Players.getLocal().isInCombat()
+                                    && !Players.getLocal().isWalking()
+                                    && currentHp() >= MaxHL*EatLowPerct
+                                    && Players.getLocal().getAnimation() == -1;
 
-            for(Npc i : Npcs.getNearest(MonsterID)) {
-                if(i != null) {
-                    if(!i.isInCombat()){
-                        return Inventory.getCount(FoodID) > 1
-                                && !Players.getLocal().isInCombat()
-                                && !Players.getLocal().isWalking()
-                                && currentHp() >= MaxHL*EatLowPerct
-                                && Players.getLocal().getAnimation() == -1;
-
-                    }
+                     }
+                  }
                 }
             }
 
@@ -75,7 +79,7 @@ public class cCombat extends Script{
             Npc[] npc = Npcs.getNearest(new Filter<Npc>() {
                 @Override
                 public boolean accept(final Npc npc) {
-                    return (npc != null && !npc.isInCombat() && npc.getDef().getId() == MonsterID);
+                    return (npc != null && npc.getDef().getId() == 0 &&!npc.isInCombat() && npc.getDef().getId() == MonsterID);
                 }
 
             });
@@ -136,7 +140,7 @@ public class cCombat extends Script{
         @Override
         public boolean activate() {
 
-            if(currentHp() >= MaxHL*EatLowPerct){
+            if(currentHp() >= MaxHL*EatLowPerct && Inventory.getCount(FoodID) >= 1){
                 if(Inventory.getCount(StrengthID) >= 1 || Inventory.getCount(AttackID) >= 1){
                     return Skill.STRENGTH.getLevel() <= Skill.STRENGTH.getRealLevel()
                             || Skill.ATTACK.getLevel() <= Skill.ATTACK.getRealLevel();
@@ -176,10 +180,62 @@ public class cCombat extends Script{
         }
     }
 
+    class Banks implements  Strategy {
+
+        @Override
+        public boolean activate() {
+            return Inventory.getCount(FoodID) < 1;
+        }
+
+        @Override
+        public void execute() {
+
+            final SceneObject[] banks = Bank.getNearestBanks();
+            SceneObject bankbooth = null;
+            if(banks.length  > 0){
+                bankbooth = banks[0];
+            }
+
+            if (bankbooth == null)  {
+                //Tab.MAGIC hook must be broken, isn't working as intended..
+                if(!Tab.MAGIC.isOpen()){
+                    Tab.MAGIC.open();
+                    sleep(500);
+                }else{
+                    Magic.clickSpell(Magic.AncientMagic377.HOME_TELEPORT);
+                    sleep(2500);
+                }
+            }else if(bankbooth != null && !Bank.isOpen()){
+                if(!Bank.getBank().isOnScreen()){
+                    Camera.turnTo(Bank.getBank());
+                    sleep(1000);
+                }else{
+                    Bank.getBank().interact("Use-quickly");
+                    sleep(5000);
+                }
+            }else if(Bank.isOpen()){
+                if(Bank.getItem(FoodID).getStackSize() >= 1){
+                    Bank.getItem(FoodID).interact("Withdraw All");
+                    sleep(1000);
+                    Bank.close();
+                }else{
+                    stopScript();
+
+
+                }
+            }
+
+        }
+    }
+
 
     public static final int currentHp() {
         String CurrentHp = Interfaces.get(3918).getChildren()[11].getText().replace("@whi@", "");
         return Integer.parseInt(CurrentHp);
+    }
+
+    public void stopScript() {
+        provide(null);
     }
 
 }
